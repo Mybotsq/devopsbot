cd847f16d1ce19d4de8b080bf3b34f0093815f34
Mybotsq <86150510+Mybotsq@users.noreply.github.com>
2024-05-20 18:36:02 +0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -311,7 +311,7 @@ def get_services(update, context):
 def get_repl_logs(update, context):
     try:
         ssh_client = get_ssh_client()
-        stdin, stdout, stderr = ssh_client.exec_command('cat /var/lib/docker/volumes/postgres_logs/_data/* | grep -A 40 "received replication command\|получена команда репликации"')
+        stdin, stdout, stderr = ssh_client.exec_command('tac /var/lib/docker/volumes/postgres_logs/_data/* | grep -m 1 "received replication command\|получена команда репликации" | head -c 4090')
         logs_status = stdout.read().decode('utf-8')
         update.message.reply_text(logs_status)
     except Exception as e:
